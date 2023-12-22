442f249aee18837275f0e2b5d9b931c0f0892bb3
Timothy Moore <mtimothy984@gmail.com>
2023-12-22 09:16:11 -0800

    Fix import test

--- test/import.js
+++ test/import.js
@@ -1,8 +1,4 @@
-import * as rqdb from '../dist/rqdb.min.js';
+import { RqliteConnection } from '../dist/rqdb.js';
 
-console.log('imported something?', rqdb);
-
-const conn = new rqdb.RqliteConnection(['http://127.0.0.1:4001']);
+const conn = new RqliteConnection(['http://127.0.0.1:4001']);
 const cursor = conn.cursor();
-
-console.log('successfully made a cursor!');
